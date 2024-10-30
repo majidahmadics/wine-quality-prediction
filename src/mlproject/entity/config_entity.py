d19495bef66ed7697e2274dc6ecc259fd3ce5980
majidahmadics <118940088+majidahmadics@users.noreply.github.com>
2024-10-30 11:58:44 +0100

    finalize data ingestion stage

---
+++ src/mlproject/entity/config_entity.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass
+from pathlib import Path
+
+# Data Ingestion Entity, which contains the configuration for data ingestion
+
+@dataclass(frozen=True)
+class DataIngestionConfig:
+    root_dir: Path
+    source_URL: str
+    local_data_file: Path
+    unzip_dir: Path
