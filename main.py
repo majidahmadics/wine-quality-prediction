3b1a11653094946d9c31150778cb6062735cda08
majidahmadics <118940088+majidahmadics@users.noreply.github.com>
2024-11-01 11:47:28 +0100

    implement and finalize data validation stage

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from mlproject import logger
 from mlproject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from mlproject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
+
+# Implementing the Data Ingestion stage
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -11,4 +14,16 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
-    
+
+
+# Impementing the Data Validation stage
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataValidationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
